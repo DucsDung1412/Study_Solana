5ba5da88da5abef054e2ca96d5e7af509aa1e62d
Zeloxpop <daoducdung2000@gmail.com>
2024-08-03 13:46:40 +0700

    setup page/index và hoàn thành home/index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
 
-const index = () => {
+import { HomeView, ToolView, FeatureView, OfferView, FaqView, AirdropView, DonateView, CreateView, InputView, TokenMetadata, ContactView } from "../views";
+
+const Home: NextPage = (props) => {
+  const [openCreateModal, setOpenCreateModal] = useState(false);
+  const [openTokenMetadata, setOpenTokenMetadata] = useState(false);
+  const [openContact, setOpenContact] = useState(false);
+  const [openAirdrop, setOpenAirdrop] = useState(false);
+  const [openSendTransaction, setOpenSendTransaction] = useState(false);
+
   return (
-    <div>index</div>
+    <>
+      <Head>
+        <title>Solana Token Creator</title>
+        <meta name='Solana token creator' content='Build and create solana token'/>
+      </Head>
+
+      <HomeView setOpenCreateModal={setOpenCreateModal} />
+      {/* <ToolView 
+        setOpenAirdrop={setOpenAirdrop}
+        setOpenContact={setOpenContact}
+        setOpenCreateModal={setOpenCreateModal}
+        setOpenSendTransaction={setOpenSendTransaction}
+        setOpenTokenMetadata={setOpenTokenMetadata}
+      />
+      <FeatureView 
+        setOpenAirdrop={setOpenAirdrop}
+        setOpenContact={setOpenContact}
+        setOpenCreateModal={setOpenCreateModal}
+        setOpenSendTransaction={setOpenSendTransaction}
+        setOpenTokenMetadata={setOpenTokenMetadata}
+      />
+      <OfferView />
+      <FaqView />
+
+      {
+        openCreateModal && (
+          <div className='new_loader relative h-full bg-state-900'>
+            <CreateView setOpenCreateModal={setOpenCreateModal} />
+          </div>
+        )
+      }
+      {
+        openTokenMetadata && (
+          <div className='new_loader relative h-full bg-state-900'>
+            <TokenMetadata setOpenTokenMetadata={setOpenTokenMetadata} />
+          </div>
+        )
+      }
+      {
+        openAirdrop && (
+          <div className='new_loader relative h-full bg-state-900'>
+            <AirdropView setOpenAirdrop={setOpenAirdrop} />
+          </div>
+        )
+      }
+      {
+        openSendTransaction && (
+          <div className='new_loader relative h-full bg-state-900'>
+            <DonateView setOpenSendTransaction={setOpenSendTransaction} />
+          </div>
+        )
+      } */}
+    </>
   )
 }
 
-export default index
+export default Home
